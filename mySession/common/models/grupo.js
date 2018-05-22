cd92fb05da34c988a202f5d02dc859daf98b5e81
Fran Marcos <franmarcos93@gmail.com>
2018-05-22 12:53:08 +0200

    Agregando hooks para asignar los propietarios

--- mySession/common/models/grupo.js
+++ mySession/common/models/grupo.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Grupo) {
-
+    	// Asignar como 'propietario' del grupo que se va a crear, al usuario que solicita su creación
+        Grupo.beforeRemote('create', function (context, Grupo, next) {
+            context.args.data.usuarioId = context.req.accessToken.userId;
+            next();
+        });
+      
 };
--- mySession/common/models/sesion.js
+++ mySession/common/models/sesion.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Sesion) {
-
+        	// Asignar como 'propietario' de la sesión que se va a crear, al usuario que solicita su creación
+            Sesion.beforeRemote('create', function (context, Sesion, next) {
+                context.args.data.usuarioId = context.req.accessToken.userId;
+                next();
+            });
+          
 };
