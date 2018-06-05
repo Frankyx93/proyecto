56658714814970e4efec5085a6fdd6b6347d34e2
Fran Marcos <franmarcos93@gmail.com>
2018-06-05 09:06:12 +0200

    pasando el user id por la url

--- mySession/client/login/scriptLogin.js
+++ mySession/client/login/scriptLogin.js
@@ -6,23 +6,14 @@ $(document).ready(function () {
     $("#iniciarSesion").click(function () {
         var email = $("#emailUsuario").val();
         var pass = $("#passwordUsuario").val();
-        console.log(email);
-        console.log(pass);
-        alert("Boton Iniciar Sesion");
         $.post("http://localhost:3000/api/Usuarios/login",
             {
                 email: email,
                 password: pass
             }).done(function (data) {
-                console.log(data.id);
                 token = data.id;
-                var idUsuarioAutenticado = data.userId
                 document.cookie = "access_token=" + token + "; expires=Thu, 18 Dec 2019 12:00:00 UTC; path=/";
-                console.log(token); 
-                //Cookies.set('access_token', data.id,{expires: 365});
-                //console.log(email); 
-                //console.log(pass);
-                location.href ="pantallasAplicacion/";
+                location.href ="pantallasAplicacion?userid="+data.userId;
             })
             .fail(function (error) {
                 console.log(error);
