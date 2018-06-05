03e8d3b2a1cc3443c5ad8b52b41548b1e80ce4bd
Fran Marcos <franmarcos93@gmail.com>
2018-06-05 10:03:09 +0200

    Añadido cliente de Fátima y script de register

--- mySession/client/login/scriptLogin.js
+++ mySession/client/login/scriptLogin.js
@@ -12,22 +12,14 @@ $(document).ready(function () {
                 password: pass
             }).done(function (data) {
                 token = data.id;
-                document.cookie = "access_token=" + token + "; expires=Thu, 18 Dec 2019 12:00:00 UTC; path=/";
-                location.href ="pantallasAplicacion?userid="+data.userId;
+                document.cookie = "access_token=" + token + "; expires=Thu, 05 Dec 2019 12:00:00 UTC; path=/";
+                //ENCONTRAR FORMA DE ENVIAR EL ID DE USUARIO
+                location.href ="app?access_token="+token;
             })
             .fail(function (error) {
                 console.log(error);
                 console.log("Credenciales incorrectas");
             })
-        // setTimeout(function () {
-        //     $.get("http://localhost:3000/api/Ejercicios?"+document.cookie)
-        //         .done(function (data) {
-        //             console.log(data);
-        //         })
-        //         .fail(function (error) {
-        //             console.log(error);
-        //         }, 2000)
-        // })
     });
 
 });
